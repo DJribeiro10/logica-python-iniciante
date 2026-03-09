1b34589c7c8420280e6082cda8a0d16256c33b62
DJribeiro10 <diogojuniorm7@gmail.com>
2026-03-08 23:38:05 -0300

    remocao

--- tabuada.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("Digite um número para ver sua tabuada: "))
-print("Tabuada do", numero)
-for i in range(1, 11):
-    resultado = numero * i
-    print(numero, "x", i, "=", resultado)
